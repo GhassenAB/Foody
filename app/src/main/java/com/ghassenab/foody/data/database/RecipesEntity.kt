f127360ae9b6ce523de767d60b9812971ed0c032
Ghassen <ghassen.abbes@gmail.com>
2020-12-31 12:39:10 +0100

    Setup Recipes Local Database

---
+++ app/src/main/java/com/ghassenab/foody/data/database/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package com.ghassenab.foody.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ghassenab.foody.models.FoodRecipe
+import com.ghassenab.foody.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+
+}
